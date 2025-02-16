b298c3b8e2ad53bb00870ba704dc5c9487e789ad
Kim Young Ju <ygjukim@gmail.com>
2025-02-16 18:01:50 +0900

    update LG Quickstart 02 project

--- pages/3_LG_Quickstart_02.py
+++ pages/3_LG_Quickstart_02.py
@@ -30,6 +30,7 @@ def LangGraph_run():
 
     graph_builder = StateGraph(State)
 
+    # Define LLM model
     claudeModelName = claudeHaikuModelName()
     chatbotModelName = modelName()
 
@@ -42,16 +43,92 @@ def LangGraph_run():
             openai_api_key=st.session_state.api_key, 
             model_name=chatbotModelName)
 
-    # Define chatbot node
+    # Define chatbot node using LLM with tools and add chatbot node to the state graph
+    os.environ["TAVILY_API_KEY"] = st.session_state.tavily_api_key
+    tool = TavilySearchResults(max_results=2)
+    tools = [tool]
+    
+    llm_with_tool = llm.bind_tools(tools)
+    
     def chatbot(state: State):
-        return {"messages": [llm.invoke(state["messages"])]}
+        # print(f"LLM 호출: {state}")
+        return {"messages": [llm_with_tool.invoke(state["messages"])]}
     
     graph_builder.add_node("chatbot", chatbot)
+
+    # Define search node using Tavily search tool and add the node to the state graph
+    import json
+    from langchain_core.messages import ToolMessage 
+
+    class BasicToolNode:
+        """ A node that runs the tools rquested in the last AIMessage."""
+        
+        def __init__(self, tools: list) -> None:
+            self.tools_by_name = {tool.name: tool for tool in tools}
+            
+        def __call__(self, inputs: dict):
+            if messages := inputs.get("messages", []):
+                message = messages[-1]
+            else:
+                raise ValueError("No messages found in inputs")
+            
+            outputs = []
+            for tool_call in message.tool_calls:
+                # print(f"Tool 실행: {tool_call}")
+                tool_result = self.tools_by_name[tool_call["name"]].invoke(tool_call["args"])
+                outputs.append(
+                    ToolMessage(
+                        content=json.dumps(tool_result),
+                        tool=tool_call["name"],
+                        tool_input=tool_call["args"],
+                        tool_call_id=tool_call["id"],
+                    )
+                )
+            
+            # print(f"Tool outputs: {outputs}")
+            return {"messages": outputs}
+
+    tool_node = BasicToolNode(tools = tools)
+    graph_builder.add_node("tool_node", tool_node)
+    
+    # Add condiftional edge between chatbot and tool node
+    from typing import Literal
+    
+    def route_tools(state: State) -> Literal["tool_node", "__end__"]:
+        """ Use in the conditional edge to route to the tool node if the last message has tool calls.
+            Otherwise, route to the end of the graph.
+        """
+        if isinstance(state, list):
+            ai_message = state[-1]
+        elif messages := state.get("messages", []):
+            ai_message = messages[-1]
+        else:
+            raise ValueError(f"No messages found in input state to tool_edge: {state}")
+       
+        if hasattr(ai_message, "tool_calls") and len(ai_message.tool_calls) > 0:
+            return "tool_node"
+        return "__end__"
+                
+    # The `tools_condition` function returns "tools" if the chatbot asks to use a tool, and "__end__" if
+    # it is fine directly responding. This conditional routing defines the main agent loop.
+    graph_builder.add_conditional_edges(
+        "chatbot",
+        route_tools,
+        # The following dictionary lets you tell the graph to interpret the condition's outputs as a specific node
+        # It defaults to the identity function, but if you
+        # want to use a node named something else apart from "tools",
+        # You can update the value of the dictionary to something else
+        # e.g., "tools": "my_tools"
+        {"tool_node": "tool_node", "__end__": "__end__"},
+    )
+
+    # Any time a tool is called, we return to the chatbot to decide the next step
+    graph_builder.add_edge("tool_node", "chatbot")
     graph_builder.add_edge(START, "chatbot")
-    graph_builder.add_edge("chatbot", END)
 
     graph = graph_builder.compile()
 
+    # Display the graph
     try:
         graph_bytes = graph.get_graph().draw_mermaid_png()
 
@@ -60,30 +137,39 @@ def LangGraph_run():
     except Exception as e:
         st.error(f"Error drawing graph: {e}")
 
-    if "messages_01" not in st.session_state:
-        st.session_state.messages_01 = []
+    # Initialize session state
+    if "messages_02" not in st.session_state:
+        st.session_state.messages_02 = []
 
     # Display all previous messages
-    for messages in st.session_state.messages_01:
+    for messages in st.session_state.messages_02:
         with st.chat_message(messages["role"]):
             st.markdown(messages["content"])
 
     # Get new user input and process it
     if prompt := st.chat_input("What is up?"):
-        st.session_state.messages_01.append({"role": "user", "content": prompt})
+        st.session_state.messages_02.append({"role": "user", "content": prompt})
         with st.chat_message("user"):
             st.markdown(prompt)
 
         # Prepare he entire chat history to senf to the model
-        full_conversation = [(m["role"], m["content"]) for m in st.session_state.messages_01]
+        full_conversation = [(m["role"], m["content"]) for m in st.session_state.messages_02]
         
         # Generate response using the model with the full conversation history
+        # i = 0
         for event in graph.stream({"messages": full_conversation}):
             for value in event.values():
-                response = value["messages"][-1].content
-                st.session_state.messages_01.append({"role": "assistant", "content": response})
-                with st.chat_message("assistant"):
-                    st.markdown(response)
+                # i = i + 1
+                # print(f"Values #{i}: {value}")
+                
+                if isinstance(value["messages"][0], AIMessage):
+                    ai_message = value["messages"][0]
+                    
+                    if not hasattr(ai_message, "tool_calls") or not ai_message.tool_calls:
+                        response = ai_message.content
+                        st.session_state.messages_02.append({"role": "assistant", "content": response})
+                        with st.chat_message("assistant"):
+                            st.markdown(response)
 
 def main():
     # add button to Sidebar to reset session state
@@ -92,7 +178,7 @@ def main():
             st.session_state.clear()
             st.rerun()      # Rerun the app to reflect the changes
 
-    st.title('LangGraph Chatbot Super Basic 🏠')
+    st.title('LangGraph Chatbot with Tools 🏠')
 
     if "model_choice" not in st.session_state:
         st.session_state.model_choice = "OpenAI ChatGPT"
@@ -111,14 +197,18 @@ def main():
             api_key = st.text_input("Please input your OpenAI API Key:", type="password")
         elif st.session_state.model_choice == "Anthropic Claude":
             api_key = st.text_input("Please input your Anthropy API Key:", type="password")
-            
+
+        if "tavily_api_key" not in st.session_state:
+            tavily_api_key = st.text_input("Please input your Tavily API Key:", type="password")
+                    
         if st.button("Submit"):
-            if api_key:
+            if api_key and tavily_api_key:
                 st.session_state.api_key = api_key
+                st.session_state.tavily_api_key = tavily_api_key
                 st.session_state.api_key_submitted = True
             else:
-                st.error("Please input your LLM API Key.")
-    
+                st.error("Please input your LLM API Key and Tavily API key.")
+                         
     if st.session_state.api_key_submitted:
         LangGraph_run()
 
